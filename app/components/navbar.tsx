75c28e82e5aa5a6b6f0e6ef9f174ce844e8f7a41
Chengkuan Zhao <95447572+chengkuanz@users.noreply.github.com>
2024-05-14 18:27:03 -0400

    nav bar added, works

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,3 +1,4 @@
+"use client"; // This is a client component
 import React from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap'
 import Link from 'next/link'
@@ -6,7 +7,7 @@ import { useRouter } from 'next/router'
 
 const NavbarComp = () => {
  // const { user, logout } = useAuth()
-  const router = useRouter()
+  //const router = useRouter()
 
   return (
       <Navbar bg="light" expand="lg">
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "@/app/components/navbar";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <body className={inter.className}>
+    <Navbar /> 
+    {children}
+    </body>
     </html>
   );
 }
