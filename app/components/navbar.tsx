a00a577e54e1ba428bcd2274269992cc7724a727
Chengkuan Zhao <95447572+chengkuanz@users.noreply.github.com>
2024-05-14 19:49:59 -0400

    fix navbar
    https://nextjs.org/docs/pages/building-your-application/upgrading/codemods#new-link

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,37 +2,29 @@
 import React from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap'
 import Link from 'next/link'
-//import { useAuth } from '../context/AuthContext'
-import { useRouter } from 'next/router'
+
 
 const NavbarComp = () => {
- // const { user, logout } = useAuth()
-  //const router = useRouter()
-
-  return (
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Link href="/" passHref>
-            <Navbar.Brand>NextJS Firebase Auth</Navbar.Brand>
-          </Link>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-
-                  <>
-                    <Link href="/signup" passHref>
-                      <Nav.Link>Signup</Nav.Link>
-                    </Link>
-                    <Link href="/login" passHref>
-                      <Nav.Link>Login</Nav.Link>
-                    </Link>
-                  </>
-
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-  )
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Link href="/" passHref>
+                    <Navbar.Brand>NextJS Firebase Auth</Navbar.Brand>
+                </Link>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Link href="/signup" passHref legacyBehavior>
+                            <Nav.Link>Signup</Nav.Link>
+                        </Link>
+                        <Link href="/login" passHref legacyBehavior>
+                            <Nav.Link>Login</Nav.Link>
+                        </Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
 }
 
 export default NavbarComp
