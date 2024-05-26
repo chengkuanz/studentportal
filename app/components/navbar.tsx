0bf9466963e29854ebc4653f6e3b04d6703de586
Chengkuan Zhao <95447572+chengkuanz@users.noreply.github.com>
2024-05-25 22:48:06 -0400

    add sample non-dropdown tab in navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -40,6 +40,9 @@ const NavbarComp = () => {
                                 </Link>
                             </>
                         )}
+                        <Link href="/login" passHref legacyBehavior>
+                            <Nav.Link>Login-SampleTab</Nav.Link>
+                        </Link>
                         <NavDropdown title="My Courses" id="basic-nav-dropdown">
                             <NavDropdown.Item href="/courses/courseList">
                                 Course List
