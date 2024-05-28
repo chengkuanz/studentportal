43471703e8c1027df0e4d2b1a65b4d7892082c9d
Le Thi Thanh Van <77523834+Le-Vivian@users.noreply.github.com>
2024-05-28 14:00:41 -0400

    show more user's info

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -4,11 +4,16 @@ import { useAuth } from '../../context/AuthContext';
 
 const User = () => {
     const { user } = useAuth();
-    const [userData, setUserData] = useState({ email: '', uid: '' });
+    const [userData, setUserData] = useState({
+        email: '',
+        uid: '',
+        firstName: '',
+        lastName: '',
+        studentNumber: '',});
 
     useEffect(() => {
         if (user) {
-            setUserData({ email: user.email, uid: user.uid });
+            setUserData({ email: user.email, uid: user.uid, firstName: user.firstName, lastName: user.lastName, studentNumber: user.studentNumber });
         }
     }, [user]);
 
@@ -26,6 +31,9 @@ const User = () => {
             }}
         >
             <h1>User Information</h1>
+            <p>First name: {userData.firstName}</p>
+            <p>Last name: {userData.lastName}</p>
+            <p>Student number: {userData.studentNumber}</p>
             <p>Email: {userData.email}</p>
             <p>User ID: {userData.uid}</p>
         </div>
