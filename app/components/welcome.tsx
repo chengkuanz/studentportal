e3b9fd9f578ec8684e8b71692656f5c0b7ced171
Yijia Xu <yijiaxu.yjx@gmail.com>
2024-06-04 15:13:48 -0400

    finished carousel

--- app/components/welcome.tsx
+++ app/components/welcome.tsx
@@ -1,35 +1,45 @@
 "use client"; // This is a client component
 import React from 'react'
-import {Carousel} from 'react-bootstrap'
+import {Button, Carousel} from 'react-bootstrap'
 import Image from 'react-bootstrap/Image';
 
 const WelcomeCarousel = () =>{
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <Carousel data-bs-theme="dark">
+            <div>
+            <Carousel >
                 <Carousel.Item>
                     <Image
                         src='/images/welcome1.jpg'
                         alt="First slide"
-                        width={1500}
+                        width={2000}
                         height={800}
-                        fluid
                     />
                     <Carousel.Caption>
-                        <h5>Start Your Italian Journey</h5>
-                        <p>button1 button2</p>
+                        <h1 class="display-1"
+                            style={{ fontFamily: "Times New Roman, Times, serif" , fontSize: "100px"}}>
+                            Start Your Italian Journey
+                        </h1>
+                        <br/> <br/>
+                        <Button variant="primary" size="lg" href="/signup">Sign Up</Button>{' '}
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <Button variant="light" size="lg" href="/login">Log in</Button>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <Image
                         src='/images/welcome2.jpg'
                         alt="Second slide"
-                        width={1500}
+                        width={2000}
                         height={800}
                         fluid
                     />
                     <Carousel.Caption>
-                        <h5>Second slide label</h5>
+                        <h1 className="display-1"
+                            style={{fontFamily: "Times New Roman, Times, serif", fontSize: "60px"}}>
+                            Watch interactive videos and immerse yourself in Italian Art & Culture
+                        </h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
@@ -37,18 +47,22 @@ const WelcomeCarousel = () =>{
                     <Image
                         src='/images/welcome3.jpg'
                         alt="Third slide"
-                        width={1500}
+                        width={2000}
                         height={800}
                         fluid
                     />
                     <Carousel.Caption>
-                        <h5>Third slide label</h5>
+                        <h1 className="display-1"
+                            style={{fontFamily: "Times New Roman, Times, serif", fontSize: "60px"}}>
+                            Master italian with our engaging learning tools
+                        </h1>
                         <p>
                             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
                         </p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
+            </div>
         </main>
     );
 }
