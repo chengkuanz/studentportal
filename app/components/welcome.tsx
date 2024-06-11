f145206e40621ae6c7f28773556e773ce499de36
Yijia Xu <yijiaxu.yjx@gmail.com>
2024-06-11 11:26:58 -0400

    adjusted welcome page features with absolute displays

--- app/components/welcome.tsx
+++ app/components/welcome.tsx
@@ -1,69 +1,79 @@
-"use client"; // This is a client component
-import React from 'react'
-import {Button, Carousel} from 'react-bootstrap'
+'use client'; // This is a client component
+import React from 'react';
+import { Button, Carousel } from 'react-bootstrap';
 import Image from 'react-bootstrap/Image';
 
-const WelcomeCarousel = () =>{
+const WelcomeCarousel = () => {
+    // Inline styles
+    const carouselItemStyle = {
+        height: '800px',
+        width: '100%'
+    };
+
+    const imageStyle = {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        minHeight: '800px',
+        width: '100%'
+    };
+
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div>
-            <Carousel >
-                <Carousel.Item>
+        <div>
+            <Carousel>
+                <Carousel.Item style={carouselItemStyle}>
                     <Image
                         src='/images/welcome1.jpg'
                         alt="First slide"
-                        width={2000}
-                        height={800}
+                        style={imageStyle}
+                        fluid
                     />
                     <Carousel.Caption>
-                        <h1 class="display-1"
-                            style={{ fontFamily: "Times New Roman, Times, serif" , fontSize: "100px"}}>
+                        <h1 className="display-1"
+                            style={{ fontFamily: "Times New Roman, Times, serif", fontSize: "100px" }}>
                             Start Your Italian Journey
                         </h1>
-                        <br/> <br/>
+                        <br /> <br />
                         <Button variant="primary" size="lg" href="/signup">Sign Up</Button>{' '}
                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                         <Button variant="light" size="lg" href="/login">Log in</Button>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item>
+                <Carousel.Item style={carouselItemStyle}>
                     <Image
                         src='/images/welcome2.jpg'
                         alt="Second slide"
-                        width={2000}
-                        height={800}
+                        style={imageStyle}
                         fluid
                     />
                     <Carousel.Caption>
                         <h1 className="display-1"
-                            style={{fontFamily: "Times New Roman, Times, serif", fontSize: "60px"}}>
+                            style={{ fontFamily: "Times New Roman, Times, serif", fontSize: "60px" }}>
                             Watch interactive videos and immerse yourself in Italian Art & Culture
                         </h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <p>Guarda video interattivi e immergiti nell'arte e nella cultura italiana</p>
                     </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item>
+                <Carousel.Item style={carouselItemStyle}>
                     <Image
                         src='/images/welcome3.jpg'
                         alt="Third slide"
-                        width={2000}
-                        height={800}
+                        style={imageStyle}
                         fluid
                     />
                     <Carousel.Caption>
                         <h1 className="display-1"
-                            style={{fontFamily: "Times New Roman, Times, serif", fontSize: "60px"}}>
-                            Master italian with our engaging learning tools
+                            style={{ fontFamily: "Times New Roman, Times, serif", fontSize: "60px" }}>
+                            Master Italian with our engaging learning tools
                         </h1>
                         <p>
-                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                            Padroneggia l'italiano con i nostri coinvolgenti strumenti di apprendimento
                         </p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
-            </div>
-        </main>
+        </div>
     );
 }
 
