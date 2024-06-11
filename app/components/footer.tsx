a322061e3c6fbf207b2578eb00f4ad35a1a9f266
Yijia Xu <yijiaxu.yjx@gmail.com>
2024-06-11 13:37:10 -0400

    added features

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,6 +1,5 @@
 'use client';
 import React from 'react';
-import {gray} from "next/dist/lib/picocolors";
 
 const Footer = () => {
     return (
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import WelcomeCarousel from "@/app/components/welcome";
 import Footer from "@/app/components/footer";
+import Features from "@/app/components/features";
 import React from "react";
 
 export default function Home() {
     return (
         <>
             <WelcomeCarousel />
+            <Features />
             <Footer />
         </>
     );
