a7b661d8be91cedcb54b796099221d30a71522d1
Chengkuan Zhao <95447572+chengkuanz@users.noreply.github.com>
2024-06-16 13:26:12 -0400

    fix layout metadaata
    Type error: Layout "app/layout.tsx" does not match the required types of a Next.js Layout.
      "getLayoutMetadata" is not a valid Layout export field.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,39 +4,33 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "@/app/components/navbar";
-import {AuthContextProvider} from "@/context/AuthContext";
+import { AuthContextProvider } from "@/context/AuthContext";
 import ProtectedRoute from "@/app/components/ProtectedRouter";
-import {usePathname} from "next/navigation";
-import {useRouter} from "next/navigation";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 import NavbarComp from "@/app/components/navbar";
 import WelcomeCarousel from "@/app/components/welcome";
 import Image from "react-bootstrap/Image";
 import React from "react";
 
-
-const noAuthRequired = ["/","/login", "/signup"];
-
+const noAuthRequired = ["/", "/login", "/signup"];
 
 const inter = Inter({ subsets: ["latin"] });
 
-
-export function getLayoutMetadata(): Metadata {
-  return {
-    title: "Language Learning Student Portal",
-    description: "Language Learning Student Portal",
-  };
-}
+// export const metadata: Metadata = {
+//     title: "Language Learning Student Portal",
+//     description: "Language Learning Student Portal",
+// };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
     const pathname = usePathname();
 
     return (
         <html lang="en">
-
         <body className={inter.className}>
         <AuthContextProvider>
             <Navbar />
@@ -47,7 +41,6 @@ export default function RootLayout({
                     {children}
                 </ProtectedRoute>
             )}
-
         </AuthContextProvider>
         </body>
         </html>
